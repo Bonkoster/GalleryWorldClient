0f62112b14e75ef9cf83eb209a9e4f14b21163c5
Bonkoster <shram3214@gmail.com>
2019-01-08 00:01:01 +0800

    Init client commit

---
+++ src/main/java/com/lukashman/GalleryWorldClientApplication.java
@@ -0,0 +1,14 @@
+package com.lukashman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryWorldClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleryWorldClientApplication.class, args);
+	}
+
+}
+
